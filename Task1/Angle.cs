1ba63f19341851c34683d94af5d6560324f01fe6
lllypik <blesphoton@gmail.com>
2022-07-18 22:45:46 +0300

    Добавьте файлы проекта.

---
+++ Task1/Angle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Angle
+    {
+        int degrees;
+        int minutes;
+        int seconds;
+        public bool isCorrect = true;
+
+        public int Degrees
+        {
+            get
+            {
+                return degrees;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    degrees = value % 360; 
+            }
+        }
+        public int Minutes
+        {
+            get
+            {
+                return minutes;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    minutes = value % 60;
+                Degrees += value / 60;
+            }
+        }
+        public int Seconds
+        {
+            get
+            {
+                return seconds;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    seconds = value % 60;
+                    Minutes += value / 60;
+            }
+        }
+
+        public Angle (int degrees, int minutes, int seconds)
+        {
+            this.Degrees = degrees;
+            this.Minutes = minutes;
+            this.Seconds = seconds;
+        }
+
+        public double ToRadians()
+        {
+            return ((degrees + minutes / 60 + seconds / (60 * 60)) * Math.PI) / 180;
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("Введите угол в формате АА.ВВ.СС (АА-градусы, ВВ - минуты, СС - секунды)");
+                int degreesAngle = Convert.ToInt32 (Console.ReadLine ());
+                int minutesAngle = Convert.ToInt32(Console.ReadLine());
+                int secondsAngle = Convert.ToInt32(Console.ReadLine());
+
+                angle = new Angle(degreesAngle, minutesAngle, secondsAngle);
+            }
+            while (angle.isCorrect == false);
+
+            Console.WriteLine("Угол {0} град. {1} мин. {2} сек. соответствует углу в {3:00.00} рад.", angle.Degrees, angle.Minutes, angle.Seconds, angle.ToRadians());
+            Console.ReadKey();
+        }
+    }
+}
